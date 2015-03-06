8977d5f09e4cc9d9c363f1b1b4f4d84cedf2c7cc
ccl2of4 <drconnoramerica@utexas.edu>
2015-03-06 01:05:58 -0600

    Update AppController.java

--- 3/src/main/java/assign/controllers/AppController.java
+++ 3/src/main/java/assign/controllers/AppController.java
@@ -23,44 +23,44 @@ public class AppController {
 	public AppServices getAppServices () {
 		return appServices == null ? (appServices = AppServices.sharedAppServices()) : appServices;}
 	
-    @RequestMapping(value = "", method=RequestMethod.GET)
-    public String index (HttpServletRequest request, HttpServletResponse response)
-    {
+	@RequestMapping(value = "", method=RequestMethod.GET)
+	public String index (HttpServletRequest request, HttpServletResponse response)
+	{
 		return "/main.jsp";
-    }
+	}
 	
-    @RequestMapping(value = "/search", params = {"type","project","year"}, method=RequestMethod.POST)
-    public String search (
-    		@RequestParam("type") String type,
-    		@RequestParam("project") String project,
-    		@RequestParam("year") String year,
-    		HttpServletRequest request)
-    {
-    	HttpSession session = request.getSession ();
-    	AppServices appServices = getAppServices ();
-    	
-    	try {
-    		String urlString = appServices.createUrlStringFromTokens (type, project, year);
-    		String pageContent = appServices.createStringFromWebPage (urlString);
-            session.setAttribute (PAGE_CONTENT, pageContent);
-        	appServices.addToHistory (session, urlString);
-        	request.getSession ().setAttribute (AppServices.HISTORY_STRING, appServices.getHistory (session));
-    	} catch (IllegalArgumentException e) {
-    		session.setAttribute (PAGE_CONTENT, errorText ("Invalid query."));
+	@RequestMapping(value = "/search", params = {"type","project","year"}, method=RequestMethod.POST)
+	public String search (
+		@RequestParam("type") String type,
+		@RequestParam("project") String project,
+		@RequestParam("year") String year,
+		HttpServletRequest request)
+	{
+		HttpSession session = request.getSession ();
+		AppServices appServices = getAppServices ();
+	    
+		try {
+			String urlString = appServices.createUrlStringFromTokens (type, project, year);
+			String pageContent = appServices.createStringFromWebPage (urlString);
+			session.setAttribute (PAGE_CONTENT, pageContent);
+			appServices.addToHistory (session, urlString);
+			request.getSession ().setAttribute (AppServices.HISTORY_STRING, appServices.getHistory (session));
+
+		} catch (IllegalArgumentException e) {
+			session.setAttribute (PAGE_CONTENT, errorText ("Invalid query."));
 		} catch (IOException e) {
 			session.setAttribute (PAGE_CONTENT, errorText ("Query returned no result."));
 		}
-    	
-    	return "/main.jsp";
-    }
-    
-    @RequestMapping(value = "/closeSession", method=RequestMethod.POST)
-    public String closeSession (HttpServletRequest request) {
-    	request.getSession ().invalidate ();
-    	return "/main.jsp";
-    }
-    
-    public static String errorText (String text) {
-    	return "<div style='color:red'>" + text + "</div>";
-    }
+		return "/main.jsp";
+	}
+
+	@RequestMapping(value = "/closeSession", method=RequestMethod.POST)
+	public String closeSession (HttpServletRequest request) {
+		request.getSession ().invalidate ();
+		return "/main.jsp";
+	}
+
+	public static String errorText (String text) {
+		return "<div style='color:red'>" + text + "</div>";
+	}
 }
